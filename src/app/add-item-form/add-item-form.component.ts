7c7f515447aaf51ab56c92c7d414b509c4536ecb
Rushang Savaliya <rushang@localhost.localdomain>
2025-03-02 21:06:42 +0530

    ng generate component AddItemForm

---
+++ src/app/add-item-form/add-item-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-form',
+  imports: [],
+  templateUrl: './add-item-form.component.html',
+  styleUrl: './add-item-form.component.scss'
+})
+export class AddItemFormComponent {
+
+}
